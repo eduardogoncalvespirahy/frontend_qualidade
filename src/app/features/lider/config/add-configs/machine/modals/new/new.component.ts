2277d5736220ec092006f3c334daeccd34a7d189
Eduardo Gonçalves Libindo <eduardogoncalves@pirahy.local>
2026-06-22 14:54:58 -0300

    modal service implementado e rember acess atualizado

--- src/app/features/lider/config/add-configs/machine/modals/new/new.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-new',
-  imports: [],
-  templateUrl: './new.component.html',
-  styleUrl: './new.component.css',
-})
-export class NewComponent {}

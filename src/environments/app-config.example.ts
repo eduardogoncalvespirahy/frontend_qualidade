1878e27fb6ca243b03ed7cba77fa6f8ef0665208
PedrodosSantos-Pirahy <pedrodossantos@pirahy.ind.br>
2026-07-02 08:45:33 -0300

    Environments resolvido

---
+++ src/environments/app-config.example.ts
@@ -0,0 +1,3 @@
+// export const appConfig = {
+//   apiUrl: 'http://192.168.53.193:3000/api'
+// };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
+import { appConfig } from './app-config';
+
 export const environment = {
   production: false,
   systemId:'1',
   // Trocar pela URL real quando o backend definir a porta/domínio
-  apiUrl: 'http://192.168.43.95:3000/api',
+  ...appConfig
 };

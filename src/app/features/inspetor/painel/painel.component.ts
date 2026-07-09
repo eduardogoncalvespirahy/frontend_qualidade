5e63fee34cb6237cadbcf2b8401b5ae29d868ea8
PedrodosSantos-Pirahy <pedrodossantos@pirahy.ind.br>
2026-07-09 08:32:32 -0300

    2

--- src/app/features/inspetor/painel/painel.component.ts
+++ src/app/features/inspetor/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+​import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { forkJoin, Observable, of } from 'rxjs';

6069de4cc9e86fb7c508a4a9dcd6481996564a85
Eduardo Gonçalves Libindo <eduardogoncalves@pirahy.local>
2026-07-13 11:26:23 -0300

    cookie

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 import { appConfig } from './app-config';
 
 export const environment = {
-  production: false,
+  production: true,
   systemId:'1',
   // Trocar pela URL real quando o backend definir a porta/domínio
   ...appConfig
